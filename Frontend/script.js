b955e33e9bcaf624e98b601034857e0d67bbd757
yakub-pollok <yakub.bdsetdemo@gmail.com>
2026-02-08 18:02:29 +0600

    js file update again

--- Frontend/script.js
+++ Frontend/script.js
@@ -17,7 +17,7 @@ function predict() {
         CHEST_PAIN: Number(CHEST_PAIN.value)
     };
 
-    fetch("https://lung-cancer-prediction-fxg7.onrender.com", {
+    fetch("https://lung-cancer-prediction-fxg7.onrender.com/predict", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
